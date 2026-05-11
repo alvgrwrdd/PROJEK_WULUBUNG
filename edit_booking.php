9901e7de3af5a95f7674d74c6cf5df2e6d1d32ed
alvgrwrdd <124250146@student.upnyk.ac.id>
2026-05-11 22:44:42 +0700

    up

--- edit_booking.php
+++ edit_booking.php
@@ -25,7 +25,7 @@ if(!$data) {
     <title>Edit Booking - Wulubung Hill</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/style2.css">
 </head>
 <body style="background-color: var(--earth-light);">
 
